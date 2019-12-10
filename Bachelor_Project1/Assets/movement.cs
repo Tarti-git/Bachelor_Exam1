6a71d0963f38732143360c790b61989a95850b5d
TartifletteVEVO <56391305+TartifletteVEVO@users.noreply.github.com>
2019-12-10 17:58:09 +0100

    Cztch object + spawn + background + handler

--- Bachelor_Project1/Assets/movement.cs
+++ Bachelor_Project1/Assets/movement.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour {
-	public int x = 0;
-	public int y = 0;
-	public int numberfinger = 0;
+	public OpenCVForUnityExample.HandPoseEstimationExample handpose;
+	public double posx = 0;
+	public double posy = 0;
+	public int nbfingers = 0;
 	public bool take;
+	public bool handler_on;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (
+		if (handler_on == true && handpose.detection_on == true) {
+			Vector3 tmppos = transform.position;
+			tmppos.x = (float)handpose.objpoint.x - 450;
+			tmppos.y = -((float)handpose.objpoint.y - 450);
+			nbfingers = handpose.numberOfFingers;
+			if (nbfingers > 3)
+				take = true;
+			else
+				take = false;
+			if (take == true) {
+				transform.position = tmppos;
+			}
+		}
+	}
+
+	public void switch_handler()
+	{
+		if (handler_on == false)
+			handler_on = true;
+		else
+			handler_on = false;
 	}
 }
---
+++ Bachelor_Project1/Assets/pizza.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pizza : MonoBehaviour {
+	
+	public OpenCVForUnityExample.HandPoseEstimationExample handpose;
+	public bool handler_on;
+	public int nbfingers = 0;
+	public int incant = 0;
+	public GameObject spawnee;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (handler_on == true && handpose.detection_on == true) {
+			Vector3 tmppos = transform.position;
+			tmppos.x = (float)handpose.objpoint.x - 450;
+			tmppos.y = -((float)handpose.objpoint.y - 450);
+			nbfingers = handpose.numberOfFingers;
+			if (handpose.numberOfFingers == 5) {
+				incant++;
+			}
+			if (incant >= 25) {
+				Instantiate (spawnee, tmppos, Quaternion.identity);
+				CancelInvoke ();
+				incant = 0;
+			}
+		}
+	}
+
+	public void switch_handler()
+	{
+		if (handler_on == false)
+			handler_on = true;
+		else
+			handler_on = false;
+	}
+}
